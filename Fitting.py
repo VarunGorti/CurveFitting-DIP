3481eefb8c4ea540e6fcacd799a1f6fb979e452c
Sriram-Ravula <sravula@login3.frontera.tacc.utexas.edu>
2023-02-26 19:11:25 -0600

    variational_dip

--- Fitting.py
+++ Fitting.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 
 import numpy as np
 
+import argparse
+
 import matplotlib.pyplot as plt
 
 from tqdm.auto import tqdm
@@ -10,6 +12,141 @@ from tqdm.auto import tqdm
 import utils
 from models import RESNET_BACKBONE, RESNET_HEAD, MODULAR_RESNET
 
+
+def dict2namespace(config):
+    namespace = argparse.Namespace()
+    for key, value in config.items():
+        if isinstance(value, dict):
+            new_value = dict2namespace(value)
+        else:
+            new_value = value
+        setattr(namespace, key, new_value)
+    return namespace
+
+class Centered_Weight_Decay(nn.Module):
+    def __init__(self, center_net, reduction="mean"):
+        super().__init__()
+
+        self.mu = nn.utils.parameters_to_vector(center_net.parameters()).detach().clone()
+        self.reduction = reduction
+
+        self.mse_loss = nn.MSELoss(reduction=self.reduction)
+    
+    def forward(self, net):
+        params_new = nn.utils.parameters_to_vector(net.parameters())
+
+        return self.mse_loss(params_new, self.mu)
+
+@torch.no_grad()
+def add_noise_to_weights(model, noise_std):
+    for param in model.parameters():
+        additive_noise = torch.randn_like(param.data) * noise_std
+        param.data.add_(additive_noise)
+
+def run_dip(args):
+    #track the outputs
+    out_list = []
+
+    mse_list_gt = []
+    mse_list_meas = []
+    mse_list_mean_gt = []
+    mse_list_mean_meas = []
+
+    reg_list = [] if args.reg_type in ["smoothing", "centered_wd"] else None
+
+    mse_fn = nn.MSELoss(reduction=args.reduction)
+    
+    #make loss fns and optimizer
+    optim = torch.optim.Adam(args.model.parameters(), lr=args.lr)
+    
+    criterion = utils.Measurement_MSE_Loss(kept_inds=args.kept_inds, per_param=True, reduction=args.reduction)
+    criterion = criterion.to(args.device)
+
+    if (args.reg_type == "smoothing") and (args.reg_lambda > 0):
+        regularizer = utils.Smoothing_Loss(per_param=True, reduction=args.reduction)
+        regularizer = regularizer.to(args.device)
+    elif (args.reg_type == "centered_wd") and (args.reg_lambda > 0):
+        center_net = args.model.backbone if args.reg_target == "backbone" else args.model
+        regularizer = Centered_Weight_Decay(center_net=center_net, reduction=args.reduction)
+        regularizer = regularizer.to(args.device)
+    elif (args.reg_type == "wd") and (args.reg_lambda > 0):
+        optim = torch.optim.Adam(args.model.parameters(), lr=args.lr, weight_decay=args.reg_lambda)
+
+    #set up some running stuff for noise
+    input_noise_start = getattr(args, 'input_noise_start', None)
+    input_noise_decay = getattr(args, 'input_noise_decay', 1.0)
+
+    perturb_weights = getattr(args, 'perturb_weights', False)
+    burn_in_iter = getattr(args, 'burn_in_iter', 0)
+    save_output_every = getattr(args, 'save_output_every', 50)
+    
+    #main loop
+    if args.debug:
+        iter_list = tqdm(range(args.num_iter))
+    else:
+        iter_list = range(args.num_iter) 
+
+    for i in iter_list:
+        optim.zero_grad()
+
+        #get the output with or without additive noise in the input
+        if (input_noise_start is not None):
+            noisy_z = args.z + torch.randn_like(args.z) * input_noise_start
+            out = args.model.forward(noisy_z)
+            input_noise_start *= input_noise_decay
+        else:
+            out = args.model.forward(args.z)
+        
+        #loss and regularization
+        error = criterion(out, args.y) 
+        
+        if (args.reg_type == "smoothing") and (args.reg_lambda > 0):
+            reg = args.reg_lambda * regularizer(out)
+            loss = error + reg
+        elif (args.reg_type == "centered_wd") and (args.reg_lambda > 0):
+            reg_target = args.model.backbone if args.reg_target == "backbone" else args.model
+            reg = args.reg_lambda * regularizer(reg_target)
+            loss = error + reg
+        else:
+            loss = error
+        
+        loss.backward()
+        optim.step()
+
+        #optional noise perturbation
+        if perturb_weights:
+            add_noise_to_weights(args.model, 2 * args.lr)
+
+        #logging and metrics
+        with torch.no_grad():
+            mse_gt = mse_fn(out, args.x).item()
+            mse_list_gt.append(mse_gt)
+
+            mse_list_meas.append(error.item())
+
+            if reg_list is not None:
+                reg_list.append(reg.item())
+            
+            if (i >= burn_in_iter) and (i % save_output_every == 0):
+                out_list.append(out.detach().clone())
+
+                mean_out = torch.mean(torch.cat(out_list), dim=0, keepdim=True)
+                
+                mse_mean_gt = mse_fn(mean_out, args.x)
+                mse_list_mean_gt.append(mse_mean_gt.item())
+
+                mse_mean_meas = criterion(mean_out, args.y)
+                mse_list_mean_meas.append(mse_mean_meas.item())
+    
+    out_dict = {"out_list": out_list,
+                "mse_list_gt": mse_list_gt,
+                "mse_list_meas": mse_list_meas,
+                "mse_list_mean_gt": mse_list_mean_gt,
+                "mse_list_mean_meas": mse_list_mean_meas}
+    if reg_list is not None:
+        out_dict["reg_list"] = reg_list
+
+    return dict2namespace(out_dict)
 
 def grab_sparams(root_pth, chip_num):
     """
@@ -52,8 +189,6 @@ def fit_DIP(model, y, z,
 
     Returns the fitted network and the final output.
     """
-    
-    #we can make the optmizer within the function since we don't need the stats between fits
     optim = torch.optim.Adam(model.parameters(), lr=lr)
     
     for i in range(num_iter):
@@ -210,7 +345,7 @@ def reptile(backbone, data_root, device, test_inds, train_inds, num_epochs, lr_m
             _, outer_test_mse = train_step(data_root=data_root, chip_num=test_chip_ind, 
                                            measurement_spacing=measurement_spacing_outer, num_measurements=num_measurements_outer, 
                                            ngf=ngf, kernel_size=kernel_size, causal=causal, passive=passive, 
-                                           backbone=backbone, device=device, lr_inner=lr_outer, 
+                                           backbone=backbone, device=device, lr_inner=lr_outer,
                                            num_iters_inner=num_iters_outer, reg_lambda_inner=reg_lambda_outer, 
                                            start_noise_inner=start_noise_outer, noise_decay_inner=noise_decay_outer)
             #update progress
@@ -231,7 +366,7 @@ def reptile(backbone, data_root, device, test_inds, train_inds, num_epochs, lr_m
             updated_net, inner_test_mse = train_step(data_root=data_root, chip_num=train_chip_ind, 
                                            measurement_spacing=measurement_spacing_inner, num_measurements=num_measurements_inner, 
                                            ngf=ngf, kernel_size=kernel_size, causal=causal, passive=passive, 
-                                           backbone=backbone, device=device, lr_inner=lr_inner, 
+                                           backbone=backbone, device=device, lr_inner=lr_inner,
                                            num_iters_inner=num_iters_inner, reg_lambda_inner=reg_lambda_inner, 
                                            start_noise_inner=start_noise_inner, noise_decay_inner=noise_decay_inner)
             #update params
--- model_parts.py
+++ model_parts.py
@@ -185,9 +185,11 @@ class UpConv(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3):
         super().__init__()
 
+        pad = (kernel_size - 1) // 2
+
         self.up_conv = nn.Sequential(
             nn.Upsample(scale_factor=2, mode='linear'),
-            nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, bias=False) 
+            nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, padding=pad, padding_mode='reflect', bias=False) 
         )
 
     def forward(self, x):
